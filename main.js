5926dd4e1c1b368738af35f87f7c56dce9546f11
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-01-09 11:12:25 +0100

    Implemented changeTipAtHover function.

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const checkBtnLm = document.getElementById('check-btn');
 const textInputLm = document.getElementById('text-input');
+   
 
+function changeTipAtHover() {
+    const lightBulbOnLm = document.getElementById('lightbulb-on');
+    const lightBulbOffLm = document.getElementById('lightbulb-off');
+    const tooltipTextLm = document.getElementById('tooltip-text');
 
-const lightBulbOnLm = document.getElementById('lightbulb-on');
-const lightBulbOffLm = document.getElementById('lightbulb-off');
-const tooltipTextLm = document.getElementById('tooltip-text');
+    lightBulbOffLm.addEventListener('mouseover', () => {
+        lightBulbOnLm.style.display = 'block';
+        tooltipTextLm.innerHTML = `<p>Examples of palindromes: <br />Civic, Radar, Race car, Never odd or even, (A man, a plan, a canal, Panama.)</p>`
+    });
+    
+    lightBulbOffLm.addEventListener('mouseout', () => {
+        lightBulbOnLm.style.display = 'none';
+        tooltipTextLm.innerHTML = `<p>A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.</p>`
+    });
+}
 
-
-lightBulbOffLm.addEventListener('mouseover', () => {
-    lightBulbOnLm.style.display = 'block';
-    tooltipTextLm.innerHTML = `<p>Examples of palindromes: <br />Civic, Radar, Race car, Never odd or even, (A man, a plan, a canal, Panama.)</p>`
-});
-
-lightBulbOffLm.addEventListener('mouseout', () => {
-    lightBulbOnLm.style.display = 'none';
-    tooltipTextLm.innerHTML = `<p>A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.</p>`
-});
-
-
-
+changeTipAtHover();
+    
 function formatString(str) {
     const regex = /[^a-zA-Z\d]/g;
     return str.replace(regex, '');
@@ -61,5 +62,8 @@ textInputLm.addEventListener('keydown', (e) => {
         isAPalindrome();
     }
 });
+
+
+
 
 
